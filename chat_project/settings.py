cc4273c2bc02e3caccb9eb16688560dd73dfff4e
barninzuniversity <ibrahimzaouali888@gmail.com>
2025-03-27 21:29:47 +0100

    Update settings.py

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-=w^d36_+==+gevx(*s3jy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,socialchat-4dod.onrender.com').split(',')
 
 
 # Application definition
